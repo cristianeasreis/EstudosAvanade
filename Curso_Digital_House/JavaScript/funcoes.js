df3c854d9692ba686502e0d01911bfb228ac39dc
cristianeasreis <cristianeasreis@hotmail.com>
2021-03-25 10:06:36 -0300

    estudandooJavaScript

---
+++ Curso_Digital_House/JavaScript/funcoes.js
@@ -0,0 +1,48 @@
+function soma(args) {
+    let result  = 0
+  for(let i = 0; i < args.length, i++;){
+      result += args[i]
+  }
+  return result
+}
+
+function main() {
+  const resultado = soma(8, 7);
+  console.log(resultado);
+}
+
+function main() {
+    const resultado = soma[5,8,15,22];
+    console.log(resultado);
+  }
+main()
+
+function subracao(a,b) {
+    return a-b
+}
+function main() {
+    const reSub = subracao(5,2)
+    console.log(reSub)
+}
+main()
+
+function soma(a,b) {
+    return a+b
+}
+function main() {
+    const reSo = soma(5,2)
+    console.log(reSo)
+}
+main()
+
+function divisao(a,b) {
+    return a/b
+}
+function main() {
+    const reDiv = divisao(5,2)
+    console.log(reDiv)
+}
+main()
+
+
+
