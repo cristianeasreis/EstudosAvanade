e468a1e5c030f1b99aec9f38fcfa94618fb3107f
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-07 14:36:32 -0300

    estuando node js

--- Curso_Digital_House/NodeJs/javascript/aulaex.js
+++ Curso_Digital_House/NodeJs/javascript/aulaex.js
@@ -1,14 +1,24 @@
-const express = require ('express');
+// const {response} = require('express')
+const { request, response } = require('express');
+const express = require('express');
 const app = express();
-const port = 3000;
-const hostname = 'localhost';
-
-app.get('/', (req, res) =>{
- const pet = ['cachorro', 'papagaio',  'gato', 'passarinho']
-    res.json(pet)
-    // res.send('HELLO WORD')
+//const port = 3000;
+//const hostname = 'localhost';
+app.get('/pets', (request, response) => {
+  const pet = ['cachorro', 'gato'];
+  return response.json(pet);
+  // res.send('HELLO WORD')
 });
-
-app.listen(port,() => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+app.post('/pets', (request, response) => {
+  return response.json(['Cachorro', 'Gato']);
 });
+app.put("/pets/:id", (request, response) => {
+  return response.json(['Dog', 'Gato']);
+});
+app.delete("/pets/:id", (request, response) => {
+  return response.json(['Cachorro']);
+});
+// app.listen(port,() => {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+// });
+app.listen(3000);
