1a4d48d8090d9f6acba495e42473b1aaf390a228
Cristiane Araujo Souza Dos Reis <48578488+cristianeasreis@users.noreply.github.com>
2021-04-08 20:36:52 -0300

    Update db.config.js

--- Curso_Digital_House/Sequelize/app/config/db.config.js
+++ Curso_Digital_House/Sequelize/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: 'localhost',
     USER: 'root',
     PASSWORD: 'Isr@el01',
-    DB: 'DB_VOLUNTARIOS',
+    DB: 'DB_Teste',
     dialect: 'mysql',
     pool: {
       max: 5,
