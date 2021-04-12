762e74d74f659f286913891818228d73aef411da
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-12 13:24:24 -0300

    estudando

--- Curso_Digital_House/aula_pet/aula_pet_node/src/index.js
+++ Curso_Digital_House/aula_pet/aula_pet_node/src/index.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const { v4: uuidv4} = require("uuid");
-
+const express = require('express');
 const app = express();
-
-app.use(express.json());
-
-const pets = [];
+const {v4 : uuidv4} = require("uuid");
+//numeros,letras e pontuações - segurança
 
 /**
 * Query params - Vamos utilizar para buscar informações específicas ou toda a informação
 * Route params - Serve para identificar um recurso na nossa rota 
-* Request body - ele serve buscar o corpo da requisição wue deve ser criada ou alterada 
+* Request body - ele serve buscar o corpo da requisição que deve ser criada ou alterada 
 */
+
+// não retorna em mais de uma rota / arquivo json - função retorna json nas demais rotas- undefined
+app.use(express.json());
+
+const pets = [];
 
 app.get('/pet',(request,response)=>{
     return response.json(pets);
 });
 
 app.post('/pet',(request,response)=>{
-    const {nome, tipoPet, idade, raca, nomeDono} = request.body;
-    const pet = {id:uuidv4(),nome, tipoPet, idade, raca, nomeDono};
+    const {nome,tipoPet,idade,raca,nomeDono} = request.body;
+    // const body = request.body; trazer corpo da req
+
+    const pet = {id: uuidv4(),nome, tipoPet,idade, raca,nomeDono};
+
     pets.push(pet);
-    return response.json(pet);// sempre retornar o pet criado e não o valor
-});
-app.put('/pet/:id',(request,response)=>{
-    return response.json();
-});
-app.delete('/pet/:id',(request,response)=>{
-    return response.json();
+
+    return response.json(pet); // sempre retornar o pet criado e não o vetor completo
 });
 
-// http://localhost:3030/pet
+app.put('/pet/:id',(request,response)=>{
+
+    return response.json();
+});
+// http://localhost:3030
 app.listen(3030);
