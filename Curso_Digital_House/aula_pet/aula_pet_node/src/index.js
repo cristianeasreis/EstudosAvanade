4020c09d1cc5b371bcfafdae83e6ae7eb897bfd1
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-12 11:21:03 -0300

    estudando

--- Curso_Digital_House/aula_pet/aula_pet_node/src/index.js
+++ Curso_Digital_House/aula_pet/aula_pet_node/src/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json());
+
+const pets = [];
+
 /**
 * Query params - Vamos utilizar para buscar informações específicas ou toda a informação
 * Route params - Serve para identificar um recurso na nossa rota 
@@ -9,7 +13,7 @@ const app = express();
 */
 
 app.get('/pet',(request,response)=>{
-    return response.json('Oi');
+    return response.json();
 });
 app.post('/pet',(request,response)=>{
     return response.json();
