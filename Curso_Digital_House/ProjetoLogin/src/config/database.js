a31a66451f965dde770fcab23ce2a7d56b8976e3
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-17 22:55:58 -0300

    estudando projeto login

---
+++ Curso_Digital_House/ProjetoLogin/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: 'mysql',
+	host: 'localhost',
+	username: 'root',
+	password: 'Isr@el01',
+	database: 'USERS',
+		define: {
+			timestamps: true,   
+			underscored: true,  
+			underscoredAll:true,
+	},
+};
