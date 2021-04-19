c7651822d0692786db7237121af88e0196ff449b
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-19 10:58:30 -0300

    login

---
+++ Curso_Digital_House/IniciandoLogin/src/routes.js
@@ -0,0 +1,10 @@
+import { request, response, Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (request, response) => {
+    return response.json({ message: 'Oi'});
+});
+
+export default routes;
+
