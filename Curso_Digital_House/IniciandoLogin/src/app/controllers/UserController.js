710bfe5c686999c850b8a5766c4190bf32c88f03
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-20 10:45:22 -0300

    login

--- Curso_Digital_House/IniciandoLogin/src/app/controllers/UserController.js
+++ Curso_Digital_House/IniciandoLogin/src/app/controllers/UserController.js
@@ -2,9 +2,21 @@ import User from '../models/User';
 
 class UserController {
     async store(request, response) {
-        const user = await User.create(request.body);
+        const userExists = await User.findOne({where: { email: request.body.email} } );
 
-        return response.json(user)
+        if(userExists){
+            return response.status(400).json ({ error: 'Usuário já existe'});
+        }
+
+
+        const {id, name, email, provider} = await User.create(request.body);
+
+        return response.json({
+            id,
+            name,
+            email,
+            provider,
+        });
     }   
 }
 
