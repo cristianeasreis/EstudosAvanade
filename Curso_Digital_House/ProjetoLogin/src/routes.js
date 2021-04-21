3daad73d22c7364a69abd26d2ea301247fc04069
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-21 10:44:47 -0300

    estudando login

--- Curso_Digital_House/ProjetoLogin/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { request, response, Router} from 'express';
-
-const routes = new Router();
-
-routes.get('/', (request, response) => {
-    return response.json({message: 'Oi'});
-});
-
-export default routes;
