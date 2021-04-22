6ce41003a3361d9d317d76bb7ebcf251b5a9c066
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-22 13:13:57 -0300

    estuandologin

--- Curso_Digital_House/IniciandoLogin/src/app/controllers/UserController.js
+++ Curso_Digital_House/IniciandoLogin/src/app/controllers/UserController.js
@@ -18,11 +18,33 @@ class UserController {
             provider,
         });
     }
-    
-    async update(request, response){
-        return response.json({ ok: true});
 
-    }
+    async update(request, response) {
+		const { email, oldPassword} =  request.body;
+
+        const user = await User.findByPk(request.userId);
+
+        if(email && email !== user.email){
+            const userExists = await User.findOne({where: { email } } );
+
+            if(userExists){
+                return response.status(400).json({error: 'Usuário já existe'})
+            }
+        }
+
+        if(oldPassword && !(await user.checkPassword(oldPassword))){
+            return response.status(401).json({erro: 'Senha não corresponde'});
+        }
+
+        const {id, name, provider}= await user.update(request.body);
+
+		return response.json({ 
+            id,
+            name,
+            email,
+            provider,
+        });
+	}
 }
 
 export default new UserController();
