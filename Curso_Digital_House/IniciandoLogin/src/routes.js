e2fc092eeff21f578a8d6d00f02297168c10c5d5
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-23 11:57:51 -0300

    estuandologin

--- Curso_Digital_House/IniciandoLogin/src/routes.js
+++ Curso_Digital_House/IniciandoLogin/src/routes.js
@@ -1,5 +1,8 @@
 import {  Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,11 +10,17 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (request, response) => {
+    return response.json({ ok: true});
+});
 
 export default routes;
