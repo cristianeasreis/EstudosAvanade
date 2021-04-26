ab53bbe515f5b6bdb9d595eae35fda2c987f0e0b
cristianeasreis <cristianeasreis@hotmail.com>
2021-04-26 13:01:50 -0300

    estudando relacionamentos

--- Curso_Digital_House/IniciandoLogin/src/app/models/File.js
+++ Curso_Digital_House/IniciandoLogin/src/app/models/File.js
@@ -7,6 +7,12 @@ class File extends Model {
 			{
 				name: Sequelize.STRING,
 				path: Sequelize.STRING,
+				url:{
+					type: Sequelize.VIRTUAL,
+					get(){
+						return `http://localhost:3030/${this.path}`;
+					}
+				}
 			},
 			{
 				sequelize,
